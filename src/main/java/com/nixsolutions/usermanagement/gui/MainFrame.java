a8c33fabb3d49770694120db670602b9cd0dd014
denis-riabiy <denys.riabyi@nure.ua>
2016-11-13 22:21:17 +0200

    6

---
+++ src/main/java/com/nixsolutions/usermanagement/gui/MainFrame.java
@@ -0,0 +1,63 @@
+package com.nixsolutions.usermanagement.gui;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+
+public class MainFrame extends JFrame {
+
+	private static final int FRAME_HEIGHT = 600;
+	private static final int FRAME_WIDTH = 800;
+	private JPanel contentPanel;
+	private JPanel browsePanel;
+
+	private MainFrame parent;
+
+	private JPanel buttonPanel;
+
+	private JTable userTable;
+
+	public MainFrame() {
+		super();
+		initialize();
+	}
+
+	private void initialize() {
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+		this.setTitle("Управление пользователями");
+		this.setContentPane(getContentPanel());
+	}
+
+	private JPanel getContentPanel() {
+		if (contentPanel == null) {
+			contentPanel = new JPanel();
+			contentPanel.setLayout(new BorderLayout());
+			contentPanel.add(getBrowsePanel(), BorderLayout.CENTER);
+		}
+		return contentPanel;
+	}
+
+	private JTable getUserTable() {
+		if (userTable == null) {
+			userTable = new JTable();
+			userTable.setName("userTable");
+		}
+		return userTable;
+	}
+
+	private JPanel getBrowsePanel() {
+		if (browsePanel == null) {
+			browsePanel = new BrowsePanel(this);
+		}
+		return browsePanel;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
